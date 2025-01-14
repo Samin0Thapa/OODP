27791a37675bee9fd898f1cd0bd8fcd29fbe2374
Samin <Samin@gmail.com>
2025-01-14 12:31:07 +0545

    Getter method added in student class

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student{
+    String name;
+    int age;
+
+
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    getInfo(){
+        system.out.println(name + age);
+
+    }
+}
